44fff5ae1ceaa8f0c8c6aae291d7ceadf7ee1698
kaaaaaaaaaaaaa <wukaiyi@cnfanews.com>
2019-11-25 18:10:25 +0800

    添加项目文件。

---
+++ samples/WKHtmltopdf.Net.Sample/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace WKHtmltopdf.Net.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
